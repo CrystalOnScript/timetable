2d365e71a36ef5a80e5a45d7a899052b3ad392e7
CrystalFaith <crystallambert@Crystals-MacBook-Pro.local>
2017-03-17 19:03:44 -0700

    updates time

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -5,7 +5,6 @@ var database = firebase.database();
 // TODO delete after de-bug
 console.log(database);
 
-
 // when clicking on the train button
 $("#trainButton").on("click", function(event){
 
@@ -32,62 +31,68 @@ $("#trainButton").on("click", function(event){
 	$("#trainFrequency").val("");
 });
 
-database.ref().on("child_added", function(snapshot) {
-
-  // Log everything that's coming out of snapshot
-  console.log(snapshot.val());
-  console.log(snapshot.val().trainTime);
-  // creates a new table row to append to the table
-  var tableRow = $("<tr>");
-  $("#trainTable").append(tableRow);
-	
-	// pulls train data from firebase
-	var trainName = snapshot.val().trainName;
-	var trainDestination = snapshot.val().trainDestination;
-	// TODO maninupulate times wiht moment.js
-	
-	var trainFrequency = snapshot.val().trainFrequency;
-
-	console.log("trainFrequency: "+ trainFrequency);
-
-	var startTime = snapshot.val().trainTime;
-	console.log(startTime)
-
-	// converts user input of trainTime to a format readable by moment.js
-	var stringTime = moment(startTime, "hh:mm a");
-
-	//turns string time into minutes
-	var minuteTime = moment().diff(stringTime, "minutes");
-
-	// modulus minute frequency and how many minutes inbetween start and now
-	var minutesModulus = minuteTime % trainFrequency;
-
-	console.log("minutesModulus: " + minutesModulus);
-
-	// subtracks the remainder minutes from frequency 
-	var timeTillNext = trainFrequency - minutesModulus;
-
-	console.log("next time:" + timeTillNext);
-
-	// displays how long in minutes to an actual time
-
-	var displayNextTrainTime = moment().add(timeTillNext, "minutes").format("LT");
-
-	
-	var trainArray = [trainName, trainDestination, trainFrequency, displayNextTrainTime, timeTillNext];
-
-	// loop to append table data to the table row
-	for(var i = 0; i <trainArray.length; i++){
+// put child_added in function - placed in set interval 
+function updateTableInterval(){
 		
-		var tableData = $("<td>");
-		tableData.text(trainArray[i]);
-		tableRow.append(tableData);
+	$("#trainTable").empty();		
+
+	database.ref().on("child_added", function(snapshot) {
+
+	var key = database.key;
+	  // creates a new table row to append to the table
+	  var tableRow = $("<tr>").attr("id", key);
+	  $("#trainTable").append(tableRow);
 		
-	};
+		// pulls train data from firebase
+		var trainName = snapshot.val().trainName;
+		var trainDestination = snapshot.val().trainDestination;
+		var trainFrequency = snapshot.val().trainFrequency;
+		var startTime = snapshot.val().trainTime;
+
+		// converts user input of trainTime to a format readable by moment.js
+		var stringTime = moment(startTime, "hh:mm a");
+
+		//turns string time into minutes
+		var minuteTime = moment().diff(stringTime, "minutes");
+
+		// modulus minute frequency and how many minutes inbetween start and now
+		var minutesModulus = minuteTime % trainFrequency;
+
+		// subtracks the remainder minutes from frequency 
+		var timeTillNext = trainFrequency - minutesModulus;
+
+		// displays how long in minutes to an actual time
+
+		var displayNextTrainTime = moment().add(timeTillNext, "minutes").format("LT");
+
+		
+		var trainArray = [trainName, trainDestination, trainFrequency, displayNextTrainTime, timeTillNext];
+
+		// loop to append table data to the table row
+		for(var i = 0; i <trainArray.length; i++){
+			
+			var tableData = $("<td>");
+			tableData.text(trainArray[i]);
+			tableRow.append(tableData);
+			
+		};
 
 
 
-  // Handle the errors
-	}, function(errorObject) {
-	  console.log("Errors handled: " + errorObject.code);
-});	
+	  // Handle the errors
+		}, function(errorObject) {
+		  console.log("Errors handled: " + errorObject.code);
+	});	
+};	
+
+// calls function to generate table
+updateTableInterval();
+
+// 
+setInterval(updateTableInterval, 60000);
+
+// Object.keys(globalObj).forEach(function(key){ console.log(key) })
+// pulls the key name
+// Object.keys(globalObj).forEach(function(key){ console.log(globalObj[key]) })
+// pulls the objects 
+
